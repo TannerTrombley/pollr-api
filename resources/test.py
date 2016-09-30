b98dd02f54b1fac2d1fd7d52fcb72d7ed9737b64
TannerTrombley <ttrom@umich.edu>
2016-09-30 12:29:00 -0400

    Hoping to get this working

--- resources/test.py
+++ resources/test.py
@@ -3,3 +3,5 @@ from flask_restful import Resource
 class Test_route(Resource):
     def get(self):
         return {'Status': 'Works!', "Messge": "This code is running on Google's scalable infrastructure. Pretty Neato!"}
+    def put(self):
+        return {'message': "This is the put route"}
