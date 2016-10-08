8a37350733de028e18ff4cd1588cd758e4303792
TannerTrombley <ttrom@umich.edu>
2016-10-07 21:20:05 -0400

    Authentication is all set on the backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+import flask_cors
 from resources import Test_route
 
 app = Flask(__name__)
+flask_cors.CORS(app)
 api = Api(app)
 
 #Add resources to the api
--- resources/test.py
+++ resources/test.py
@@ -1,8 +1,17 @@
 from flask_restful import Resource
+from flask import request
+from common import verify_auth_token
 
 class Test_route(Resource):
+
     def get(self):
-        return {'Status': 'Works!', "Messge": "This code is running on Google's scalable infrastructure. Pretty Neato!"}
+        claims = verify_auth_token(request)
+        res = {}
+        if not claims:
+            return {"error": "Unauthorized"}, 401
+
+        return claims, 200
+
     def put(self):
         return {'message': "This is the put route"}
 
