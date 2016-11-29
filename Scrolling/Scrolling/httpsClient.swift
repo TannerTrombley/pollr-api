06de1dac70ca84ba9589eb6f5c3d57922d23c19f
David Hendershot <davidhe@umich.edu>
2016-11-28 20:08:39 -0500

    Cleaned up the clientAPI class a bit

--- Scrolling/Scrolling/httpsClient.swift
+++ Scrolling/Scrolling/httpsClient.swift
@@ -11,82 +11,71 @@ import SwiftyJSON
 import FBSDKLoginKit
 import Firebase
 
+
 class clientAPI {
 	var authToken : String
 	
+	// Only constructor requires a Firebase authorization token
 	init(token: String) {
 		self.authToken = token
 	}
 	
+
+	
+	// MARK: GET requests
+	
+	// Retrieve spoofed polls for demonstration purposes
 	func getDemoPolls(done:@escaping ([Poll]) -> Void) {
 		let header : HTTPHeaders = [
 			"Authorization": authToken,
 		]
 		
 		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/demo", headers: header).responseJSON { response in
-			
 			if let result = response.result.value {
-				let json = JSON(result)
-				
-				let jsonPolls = json["result"]
-				
-				var polls = [Poll]()
-				
-				for p in jsonPolls {
-					
-					let pollId = p.1["id"].intValue
-					let question = p.1["question"].stringValue
-					let voted = p.1["voted"].boolValue
-					
-					var responses = [String: Int]()
-					for answer in p.1["answers"] {
-						responses[answer.1["answer_text"].stringValue] = p.1["answers_count"].intValue
-					}
-					
-					let newPoll = Poll(id: pollId, question: question, answers: responses, voted: voted)
-					polls.append(newPoll)
-				}
-				
+				let polls = self.parsePolls(serverResponse: JSON(result))
 				done(polls)
 			}
 		}
 	}
 	
+	// Parses the JSON and returns a list of the found Polls
+	func parsePolls(serverResponse: JSON) -> [Poll] {
+		
+		let pollData = serverResponse["results"]
+		var polls = [Poll]()
+		
+		for p in pollData {
+			let pollId = p.1["id"].intValue
+			let question = p.1["question"].stringValue
+			let voted = p.1["voted"].boolValue
+			
+			var responses = [String: Int]()
+			for answer in p.1["answers"] {
+				responses[answer.1["answer_text"].stringValue] = p.1["answers_count"].intValue
+			}
+			
+			let newPoll = Poll(id: pollId, question: question, answers: responses, voted: voted)
+			polls.append(newPoll)
+		}
+		
+		return polls
+	}
+	
+	// Retrieve the polls a particular user has created
 	func getMyPolls(done:@escaping ([Poll]) -> Void) {
 		let header : HTTPHeaders = [
 			"Authorization": authToken,
 			]
 		
 		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/user/polls", headers: header).responseJSON { response in
-			
 			if let result = response.result.value {
-				let json = JSON(result)
-				
-				let jsonPolls = json["result"]
-				
-				var polls = [Poll]()
-				
-				for p in jsonPolls {
-					
-					let pollId = p.1["id"].intValue
-					let question = p.1["question"].stringValue
-					
-					let voted = p.1["voted"].boolValue
-					
-					var responses = [String: Int]()
-					for answer in p.1["answers"] {
-						responses[answer.1["answer_text"].stringValue] = p.1["answers_count"].intValue
-					}
-					
-					let newPoll = Poll(id: pollId, question: question, answers: responses, voted: voted)
-					polls.append(newPoll)
-				}
-				
+				let polls = self.parsePolls(serverResponse: JSON(result))
 				done(polls)
 			}
 		}
 	}
 	
+	// Retrieve all polls within a certain radius of the user's current location
 	func getPolls(latitude: Double, longitude: Double, done:@escaping ([Poll]) -> Void) {
 		let header : HTTPHeaders = [
 			"Authorization": authToken,
@@ -100,34 +89,50 @@ class clientAPI {
 		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/location/polls", parameters: parameters, headers: header).responseJSON { response in
 			
 			if let result = response.result.value {
-				let json = JSON(result)
-				
-				let jsonPolls = json["result"]
-				
-				var polls = [Poll]()
-				
-				for p in jsonPolls {
-					
-					let pollId = p.1["id"].intValue
-					let question = p.1["question"].stringValue
-					let voted = p.1["voted"].boolValue
-					
-					var responses = [String: Int]()
-					for answer in p.1["answers"] {
-						responses[answer.1["answer_text"].stringValue] = p.1["answers_count"].intValue
-					}
-					
-					let newPoll = Poll(id: pollId, question: question, answers: responses, voted: voted)
-					polls.append(newPoll)
-				}
-				
+				let polls = self.parsePolls(serverResponse: JSON(result))
 				done(polls)
 			}
 		}
 	}
-
+	
+	// Retrieve a particular poll for further analysis
+	func getPoll(pollId: Int, done: @escaping (Poll) -> Void) {
+		let header : HTTPHeaders = [
+			"Authorization": authToken
+			]
+		
+		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/polls/" + String(pollId), headers: header).responseJSON { response in
+			if let result = response.result.value {
+				let polls = self.parsePolls(serverResponse: JSON(result))
+				if let poll = polls.first {
+					done(poll)
+				}
+			}
+		}
+	}
+	
+	// Retrieve the user's points
+	func getPoints(done: @escaping (Int) -> Void) {
+		
+		let headers : HTTPHeaders = [
+			"Authorization": authToken,
+			]
+		
+		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/user", method: .get, headers: headers).responseJSON { response in
+			if let result = response.result.value {
+				let json = JSON(result)
+				let points = json["result"]["user_points"].intValue
+				done(points)
+			}
+		}
+	}
 	
 	
+	
+	
+	// MARK: POST requests
+	
+	// Create a poll with the given particulars
 	func createPoll(question : String, answers : [String], latitude: Double, longitude: Double) {
 		
 		let headers : HTTPHeaders = [
@@ -135,10 +140,7 @@ class clientAPI {
 			"Content-Type": "application/json"
 		]
 		
-		var answers_counts = [Int]()
-		for _ in 0..<answers.count {
-			answers_counts.append(0)
-		}
+		let answer_counts = [Int](repeating: 0, count: answers.count)
 		
 		let parameters : Parameters = [
 			"question": question,
@@ -146,38 +148,13 @@ class clientAPI {
 			"lon": longitude,
 			"radius": 8000,
 			"answers": answers,
-			"answer_counts": answers_counts
+			"answer_counts": answer_counts
 		]
 		
 		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/polls", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
 	}
 	
-	func getPoll(pollId: Int, done: @escaping (Poll) -> Void) {
-		let header : HTTPHeaders = [
-			"Authorization": authToken
-			]
-		
-		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/polls/" + String(pollId), headers: header).responseJSON { response in
-			
-			if let result = response.result.value {
-				let json = JSON(result)
-				let poll = json["result"]
-			
-				var responses = [String: Int]()
-				for answer in poll["answers"] {
-					responses[(answer.1["answer_text"].stringValue)] = answer.1["count"].intValue
-				}
-				
-				let pollId = poll["id"].intValue
-				let question = poll["question"].stringValue
-				
-				let newPoll = Poll(id: pollId, question: question, answers: responses, voted: false)
-
-				done(newPoll)
-			}
-		}
-	}
-	
+	// Place a single vote for the specified answer on the specified poll
 	func voteOnPoll(pollId: Int, userVote: String) {
 		
 		let headers : HTTPHeaders = [
@@ -190,22 +167,6 @@ class clientAPI {
 		]
 		
 		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/polls/" + String(pollId), method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-		}
-	}
-	
-	func getPoints(done: @escaping (Int) -> Void) {
-		
-		let headers : HTTPHeaders = [
-			"Authorization": authToken,
-		]
-		
-		Alamofire.request("https://pollr-api.appspot.com/api/v1.0/user", method: .get, headers: headers).responseJSON { response in
-			if let result = response.result.value {
-				let json = JSON(result)
-				let points = json["result"]["user_points"].intValue
-				
-				done(points)
-			}
 		}
 	}
 }
